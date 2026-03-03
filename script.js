77f8f42d9f548430f10d352bb3c3835c46ba4d0e
Guilherme Bernardo Hartmann <guilhermehartmann458@gmail.com>
2026-03-02 21:17:12 -0300

    funcionalidade calculando produto javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+function calcularTotal(){
+    const checkboxes = document.querySelectorAll('.item-produto input[type="checkbox"]');
+    const qtds = document.querySelectorAll('.qtd-produto');
+
+    let total = 0;
+
+    checkboxes.forEach((checkbox, index) => {
+        if (checkbox.checked) {
+            const valor = parseFloat(checkbox.value);
+            const qtd = parseInt(qtds[index].value);
+            total += valor * qtd;
+        }
+    });
+
+    document.getElementById('valor-total').innerText = total.toFixed(2);
+}
+
+const checkboxes = document.querySelectorAll('.item-produto input[type="checkbox"]');
+const qtds = document.querySelectorAll('.qtd-produto');
+
+checkboxes.forEach(cb => cb.addEventListener('change', calcularTotal));
+qtds.forEach(qtd => qtd.addEventListener('change', calcularTotal));
+
+
+calcularTotal();
+
